9129e68e0aa0b472b3b91efc5891b7f9ce1463bd
Faheem-ahamed3105 <112719120+Faheem-ahamed3105@users.noreply.github.com>
2022-09-21 09:57:20 +0530

    Add files via upload

---
+++ sumofgivennumbers.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+class gfg 
+{
+public:
+
+    int n;
+
+    {
+
+        int sum = 0;
+
+        while (n != 0) {
+
+            sum = sum + n % 10;
+
+            n = n / 10;
+
+        }
+
+        return sum;
+
+    }
+};
+int main()
+{
+
+    gfg g;
+
+    int n = 687;
+
+    cout << g.getSum(n);
+
+    return 0;
+}
